a8db85499de02c6ab0849cabc3f3e75f3ffacb11
= <=>
2025-09-08 20:36:21 +0530

    q1 done

---
+++ build/CMakeFiles/ISSassg1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ISSassg1.dir/main.cpp.o"
+  "CMakeFiles/ISSassg1.dir/main.cpp.o.d"
+  "ISSassg1"
+  "ISSassg1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ISSassg1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- user_code.h
+++ user_code.h
@@ -7,6 +7,7 @@
 #include <utility>
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -15,9 +16,127 @@ using namespace std;
 Complete this function
 */
 
+vector<vector<int>> sortByParcelId(const vector<vector<int>>& input_parcels) {
+    vector<vector<int>> sorted_parcels = input_parcels;
+    // sort using the 1st element of each vector (extend the std::sort() with custom comparison logic[lambda function])
+    std::sort(sorted_parcels.begin(), sorted_parcels.end(), [](const vector<int>& a, const vector<int>& b) {
+        
+        //if ID is same then sort by weight
+        if(a[0] == b[0]) {
+            return a[1] < b[1];
+        }
+        else
+        {return a[0] < b[0];}
+    });
+
+    //return vector (sorted by parcel ID and weight)
+    return sorted_parcels;
+
+}
+
 vector<vector<int>> question_one(const vector<vector<int>>& parcels) {
     // TODO: Implement function
+    // parcels[i][0] is id of parcel i
+    // parcels[i][1] is weight of parcel i
+    // id might be repeated for different parcels
+
+    // // approach in which the input is sorted first and then duplicates are found
+    // cout<<"Input Parcels: "<<endl;
+    // for(auto parcel : parcels) {
+    //     cout <<"Id " << parcel[0] << " Weight " << parcel[1] << endl;
+    // }   
+    // cout<<endl;
+
+    // cout<<"Sorted Parcels: "<<endl;
+    // auto sorted = sortByParcelId(parcels);
+    // for(auto parcel : sorted) {
+    //     cout <<"Id " << parcel[0] << " Weight " << parcel[1] << endl;
+    // }   
+    // cout<<endl;
+
+    // //return sorted;
+
+    // //initialize result vector
+    // vector<vector<int>> result; // {id, min_weight}
+    // size_t n = sorted.size();      // for loop length
+    // size_t i = 0;                  // iterator
+    // size_t j = i;
+    // int id, min_weight, count;
+    // bool has_duplicate;         // flag to check if duplicate exists
+
+    // while (i < n) {
+    //         id = sorted[i][0];
+    //         min_weight = sorted[i][1];
+    //         j = i + 1;
+    //         has_duplicate = false;
+
+    //         while (j < n && sorted[j][0] == id) {
+    //             min_weight = min(min_weight, sorted[j][1]);
+    //             has_duplicate = true;
+    //             j++;
+    //         }
+
+    //         if (has_duplicate) {
+    //             result.push_back({id, min_weight});
+    //         }
+
+    //         i = j;
+    //     }
+
+    // // print out the result
+
+    // cout<<"Duplicate Parcels with Minimum Weight: "<<endl;
+    // for(auto parcel : result) {
+    //     cout <<"Id " << parcel[0] << " Min Weight " << parcel[1] << endl;
+    // }   
+    // cout<<endl; 
+
+
+
+    //approach which skips the sorting part and directly finds duplicates
+
+    //using unordered_map to store min weights and counts of each id
+    unordered_map<int, int> min_weights;
+    unordered_map<int, int> counts;
+    int id, weight;
+
+    //sort the parcels to ensure ordering by ID in the result vector
+    //this is optional, if ordering is not required, this can be skipped
+    auto sorted = sortByParcelId(parcels); 
+
+    for (const auto& parcel : sorted) {
+        id = parcel[0], weight = parcel[1];
+        counts[id]++;
+        if (min_weights.count(id)) {
+            min_weights[id] = min(min_weights[id], weight);
+        } else {
+            min_weights[id] = weight;
+        }
+    }
+
+    vector<vector<int>> result;
+    for (const auto& [id, min_weight] : min_weights) {
+        if (counts[id] > 1) {
+            result.push_back({id, min_weight});
+        }
+    }
+
+    // print out the result
+
+    cout<<"Duplicate Parcels with Minimum Weight: "<<endl;
+    for(auto parcel : result) {
+        cout <<"Id " << parcel[0] << " Min Weight " << parcel[1] << endl;
+    }   
+    cout<<endl; 
+
+
+    return result;  
+
+
+
 }
+
+
 
 
 
@@ -32,6 +151,7 @@ vector<int> question_two(
     const vector<vector<int>>& query
 ) {
     // TODO: Implement function
+    return{};
 }
 
 
@@ -45,6 +165,7 @@ long long question_three(
     const vector<int>& metro_cities
 ) {
     // TODO: Implement function
+    return 0;
 }
 
 
